31b9f17a53afc3b9d6cd912ee7d1385b57c9ae15
Mushegh <musheghdavtyan@gmail.com>
2021-08-08 17:21:35 +0400

    Update kubernetes certificate renewer.sh

--- kubernetes certificate renewer.sh
+++ kubernetes certificate renewer.sh
@@ -4,7 +4,6 @@
 This script will renew the all kubernetes certificates.
 '
 
-
 minimum_days="7"
 
 
@@ -18,6 +17,7 @@ red=$(tput setaf 1)
 green=$(tput setaf 76)
 tan=$(tput setaf 3)
 blue=$(tput setaf 38)
+expired="yes"
 
 e_header() { printf "${bold}${purple}==========  %s  ==========${reset}\n" "$@" 
 }
@@ -52,25 +52,34 @@ usage()
 
 residual_days_counter_new()
 {
-	residual_days=`kubeadm certs check-expiration --skip-headers  --skip-log-headers \
+	if [[ ! -f "/tmp/kubeadm" ]]; then
+		curl -L -o /tmp/kubeadm https://dl.k8s.io/release/v1.20.1/bin/linux/amd64/kubeadm --insecure > /dev/null 2>&1;
+	    chmod +x /tmp/kubeadm
+	fi
+	chmod +x /tmp/kubeadm
+	residual_days=`/tmp/kubeadm certs check-expiration --skip-headers  --skip-log-headers \
 	| grep -Ev 'EXPIRES|AUTHORITY|Reading|FYI:' \
 	| awk '{print $7}' | sed 's/d/\td/' \
 	| sed 's/y/\ty/' | sort -t: -u -k1,1 \
-	| grep d | awk '{print $1}' | head -1`
-	echo $residual_days
+	| grep d | awk '{print $1}' | head -1` > /dev/null 2>&1;
+	if [[ "$residual_days" == *"nval"* ]]; then
+		echo "1"
+	else
+		expired="no"
+		echo $residual_days
+	fi
 }
 
 forceupdate()
 {
 	e_warning "There are still $(residual_days_counter_new) days to certificate expiration."
-	echo ""
 	e_warning "Do you want to force renew kubernetes certificates? [yes/no]"
 	read answer
 	if [[ "$answer" == "yes" ]]
 	then 
 		force="yes"
 	else
-		e_warning "Aborting"
+		e_warning "Aborting..."
 		exit 0
 	fi
 }
@@ -79,7 +88,7 @@ forceupdate()
 while getopts "k:h:f:" o; do
     case "${o}" in
         k)
-            kubeconfig=${OPTARG}
+            kubeconf=${OPTARG}
             ;;
         h)
             usage
@@ -95,79 +104,76 @@ done
 shift $((OPTIND-1))
 
 
-if [ -z "${kubeconfig}" ]; then
-	e_warning "kubeconfig is /etc/kubernetes/admin.conf"
-	echo ""
-	echo ""
-    kubeconf=/etc/kubernetes/admin.conf
+e_header "Starting the script"
+sleep 1
+if [ -z "${kubeconf}" ]; then
+	if [[ -f "/etc/kubernetes/admin.conf" ]]; then
+		e_warning "kubeconfig is /etc/kubernetes/admin.conf"
+	    kubeconf=/etc/kubernetes/admin.conf
+	else
+		echo "cannot find kubeconfig"
+		exit 1
+    fi
     sleep 1
 fi
 
-e_header "Starting the script"
-echo ""
-sleep 1
-node=`kubectl --kubeconfig $kubeconf get nodes --no-headers | head -1 | awk '{print $1}'`
-version=`kubectl --kubeconfig $kubeconf get node $node -o jsonpath='{.status.nodeInfo.kubeletVersion}' | sed 's/v//' | tr -d .`
-e_success "kubernetes version is `kubectl --kubeconfig $kubeconf get node $node -o jsonpath='{.status.nodeInfo.kubeletVersion}'`"
-sleep 1
 todayyear=$(date +"%Y")
 todaymonth=$(date +"%m")
 todayday=$(date +"%d")
 
 
-e_warning "Certificates will expire after $(residual_days_counter_new) days."
-
-
 backup_creator()
 {
-	mkdir -p $HOME/old-certs/pki
-	mkdir -p $HOME/old-certs/.kube
-	\cp -rp /etc/kubernetes/pki/*.* $HOME/old-certs/pki
-	\cp -rp /etc/kubernetes/*.conf $HOME/old-certs
-	\cp -rp ~/.kube/config $HOME/old-certs/.kube/.
+	backup_directory="$HOME/kubernetes-$todayyear-$todaymonth-$todayday"
+	mkdir -p $backup_directory
+	\cp -rp /etc/kubernetes $backup_directory/kubernetes
+	\cp -rp /var/lib/kubelet $backup_directory/kubelet
 }
 
 
 kubelet_restart()
 {
-  	    systemctl daemon-reload&&systemctl restart kubelet
-	    servicecheck=`systemctl is-active kubelet`
-	    if [[ "$servicecheck" != "active" ]]
-        then
-            sleep 10
-			checkcount=10
-			while [[ "$servicecheck" != "active" ]];
-			do 
-			checkcount=$((checkcount-1))
 
-			if [[ $checkcount != 0 ]]
-			then 
-				e_warning "Waiting for kubelet to become active. Try count $checkcount" && sleep 3;
-			else
-			    e_error "Kubelet state is down. Exiting !!!"
-			    exit 1
-			fi
-			done
-			e_success "Kubelet is ready!"
+	e_warning "restarting the kubelet service"
+	systemctl daemon-reload && systemctl restart kubelet
+	sleep 5
+    servicecheck=`systemctl is-active kubelet`
+    if [[ "$servicecheck" != "active" ]]
+    then
+        sleep 10
+		checkcount=10
+		while [[ "$servicecheck" != "active" ]];
+		do 
+		checkcount=$((checkcount-1))
+
+		if [[ $checkcount != 0 ]]
+		then 
+			e_warning "Waiting for kubelet to become active. Try count $checkcount" && sleep 3
 		else
-			e_success "Kubelet is ready!"
+		    e_error "Kubelet state is down. Exiting !!!"
+		    exit 1
 		fi
+		done
+		e_success "Kubelet is ready!"
+	else
+		e_success "Kubelet is ready!"
+	fi
 }
 
 
 kubelet_checker()
 {
     e_purple "Checking kubelet service availability and new certificates"
-	DIFF=$(diff $HOME/old-certs/kubelet.conf /etc/kubernetes/kubelet.conf) 
+	DIFF=$(diff $backup_directory/kubernetes/kubelet.conf /etc/kubernetes/kubelet.conf) 
     if [ "$DIFF" != "" ] 
     then
     	e_success "!! Success. Kubelet config also renewed. Trying to restart kubelet"
     	kubelet_restart
 	else
         cd /etc/kubernetes
-        mv kubelet.conf $HOME/old-certs/kubelet.conf.bak
-        \cp -r admin.conf kubelet.conf	
-	         kubelet_restart
+        mv /etc/kubernetes/kubelet.conf $backup_directory/kubernetes/kubelet.conf.bak
+        \cp -r /etc/kubernetes/admin.conf /etc/kubernetes/kubelet.conf	
+	    kubelet_restart
 	fi
 }
 
@@ -175,7 +181,7 @@ kubelet_checker()
 kubeconfig_checker()
 {
     e_purple "Checking kubeconfig availability and new certificates"
-	check=`kubectl --kubeconfig $kubeconf get ns | grep default | awk '{print $1}'`
+	check=`kubectl --kubeconfig ~/.kube/config get ns | grep default | awk '{print $1}'`
     if [ "$check" == "default" ]
     then
     	e_success "Kubeconfig is working"
@@ -186,44 +192,66 @@ kubeconfig_checker()
 	fi
 }
 
-if [[ "$version" < 1150 ]]
+kube_cert_updater()
+{
+	e_purple "Starting certificate update process"
+	sleep 1
+    backup_creator
+    sleep 1
+    if [[ "$expired" != "yes" ]]; then
+		node=`kubectl --kubeconfig $kubeconf get nodes --no-headers | head -1 | awk '{print $1}'`
+		version=`kubectl --kubeconfig $kubeconf get node $node -o jsonpath='{.status.nodeInfo.kubeletVersion}' | sed 's/v//' | tr -d .`
+		e_success "kubernetes version is `kubectl --kubeconfig $kubeconf get node $node -o jsonpath='{.status.nodeInfo.kubeletVersion}'`"
+	fi
+	sleep 1
+    advertise=`cat /etc/kubernetes/kubelet.conf | grep server | awk '{print $2}' | sed -e 's/https:\/\///' | sed -e 's/:6443//'`
+    e_purple "Setting the advertise address $advertise"
+    sleep 1
+	if [[ ! -f "/tmp/kubeadm" ]]; then
+		e_purple "Downloading the kubeadm"
+		curl -L -o /tmp/kubeadm https://dl.k8s.io/release/v1.20.1/bin/linux/amd64/kubeadm --insecure > /dev/null 2>&1;
+	    chmod +x /tmp/kubeadm
+	fi
+    e_purple "Stopping kubelet service"
+    systemctl stop kubelet
+    sleep 5
+    e_purple "Renewing the certificates"
+    /tmp/kubeadm alpha certs renew all > /dev/null 2>&1;
+	e_success "alpha certs were updated"
+	sleep 1
+	e_purple "Starting conf file update process"
+	cd /etc/kubernetes 
+    rm -rf *.conf
+    /tmp/kubeadm init phase kubeconfig all --apiserver-advertise-address $advertise > /dev/null 2>&1;
+	rm -rf ~/.kube/config
+	cp -r /etc/kubernetes/admin.conf ~/.kube/config > /dev/null 2>&1;
+	chown $(id -u):$(id -g) ~/.kube/config > /dev/null 2>&1;
+	sleep 5
+	rm -rf /var/lib/kubelet/pki/kubelet.key
+	rm -rf /var/lib/kubelet/pki/kubelet.crt
+    kubelet_checker
+    if [[ "$expired" != "yes" ]]; then
+    	kubeconfig_checker
+    fi
+    days=$(residual_days_counter_new)
+    if [[ $days > 300 ]]
+    then
+    	e_success "All certificates have been renewed"
+    	e_success "Certificates will expire after $days days"
+    	e_warning "Rebooting the server"
+    	shutdown -r > /dev/null 2>&1;
+    	exit 0
+    fi	
+}
+
+days=$(residual_days_counter_new)
+if [[ "$days" -lt "$minimum_days" ]] || [[ "$force" == "yes" ]]
 then
-	kube_version=old
+	kube_cert_updater
 else
-	kube_version=new
+	e_arrow "There is still $days to certificate expiration"
+	echo \n
+	e_header "No need to update certificates at this moment"
+	echo ""
+	exit 0
 fi
-
-if [[ "$kube_version" == "new" ]]
-then
-	days=$(residual_days_counter_new)
-	if [ "$days" -lt "$minimum_days" ] || [[ "$force" == "yes" ]]
-	then
-		e_purple "Starting certificate update process"
-		sleep 1
-        backup_creator
-        sleep 1
-		kubeadm certs renew all > /dev/null 2>&1;
-		e_success "alpha certs were updated"
-		sleep 1
-		cd /etc/kubernetes/
-		e_purple "Starting conf file update process"
-		kubeadm init phase kubeconfig all > /dev/null 2>&1;
-		\cp -r /etc/kubernetes/admin.conf $HOME/.kube/config > /dev/null 2>&1;
-		chown $(id -u):$(id -g) $HOME/.kube/config
-		sleep 5
-        kubelet_checker
-        kubeconfig_checker
-        days=$(residual_days_counter_new)
-        if [[ $days > 300 ]]
-        then
-        	e_success "All certificates have been renewed"
-        	exit 0
-        fi	
-	else
-        e_arrow "There is still $days to certificate expiration"
-        echo \n
-        e_header "No need to update certificates at this moment"
-        echo ""
-	fi    
-fi
-
